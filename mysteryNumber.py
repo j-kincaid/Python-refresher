2a7f3cc877a3ed7d3fb3e982f5ffa4d38f6fc05d
Jessica Kincaid <4j.kincaid@gmail.com>
2021-11-22 14:47:58 -0600

    add details to clue the user in to how the game works

--- mysteryNumber.py
+++ mysteryNumber.py
@@ -1,21 +1,22 @@
 import random
 
 
- """
- Feedback: The first printed statement should clue the user 
- in to what the program is doing. If they choose 1 in the beginning
- then the random number will be picked between 1 and 1, it
- will always be 1. It should say something more like
-A random number will be picked between 1 and the number you input 
-or something along those lines.
- """  
-top_of_range = input("Type a number between 1 and 10: ")
+#  """
+#  Feedback: The first printed statement should clue the user 
+#  in to what the program is doing. If they choose 1 in the beginning
+#  then the random number will be picked between 1 and 1, it
+#  will always be 1. It should say something more like
+# A random number will be picked between 1 and the number you input 
+# or something along those lines.
+#  """  
+top_of_range = input("Thank you for playing the Mystery number game! Type a number between 1 and 100: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
 rNum = random.randint(1, top_of_range)
 guesses = 0
+print('You just chose a number. That is the greatest of the numbers that will be randomized. Try to guess the random number in 7 guesses or fewer.')
 while True:
     guesses += 1
     user_guess = input("Make a guess: ")
