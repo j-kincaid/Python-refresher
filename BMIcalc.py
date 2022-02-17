564cc14d307add36372f8fa5a1ca0373b92504bb
Jessica Kincaid <4j.kincaid@gmail.com>
2022-02-17 10:11:25 -0600

    complete bmi calculator

--- BMIcalc.py
+++ BMIcalc.py
@@ -1,2 +1,11 @@
 # BMI = weight / height ** 2
 
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+h = float(height)
+w = float(weight)
+bmi = str(int(w / (h * h)))
+
+print(bmi)
