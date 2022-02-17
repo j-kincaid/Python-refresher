f17ec34d3e440356e0589e55dfe51ab17eaedde8
Jessica Kincaid <4j.kincaid@gmail.com>
2022-02-17 10:29:03 -0600

    practice decimals

---
+++ decimals.py
@@ -0,0 +1,13 @@
+print(type(8 / 3))
+# <class 'float'>
+
+print(round(8/3, 2))
+# rounds result to 2 decimal places
+
+print(8 // 3)
+# floor division = int
+
+result = 4 / 2 
+result /= 2
+print(result)
+# 1.0
