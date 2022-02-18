ed168358bae6e434b3968add4bc9f34e88ee19ae
Jessica Kincaid <4j.kincaid@gmail.com>
2022-02-18 15:14:33 -0600

    divide tip by 100 before multiplying times bill

---
+++ tipCalc.py
@@ -0,0 +1,18 @@
+# Welcome to the tip calculator. 
+# What was the total bill? $124.56
+# What percentage tip would you like to give? 10, 12, or 15? 12
+# How many people to split the bill? 7
+# Each person should pay: $19.93
+
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill?"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+# grand_total = bill + tip + 1.0
+party_size = int(input("How many people to split the bill?"))
+bill_with_tip = tip / 100 * bill + bill
+payment_per_guest = round((bill_with_tip / party_size), 2)
+message = f"Each person should pay: ${payment_per_guest}"
+print(message)
+
+
+# It's still not right!!!
