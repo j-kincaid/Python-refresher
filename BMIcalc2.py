ba99363340964ed1fed08d41d950634352e4460a
Jessica Kincaid <4j.kincaid@gmail.com>
2022-02-19 11:34:43 -0600

    fix BMIcalc2

--- BMIcalc2.py
+++ BMIcalc2.py
@@ -4,9 +4,24 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-h = float(height)
-w = float(weight)
-bmi = int(w / (h * h))
+# h = round(height, 2)
+# w = round(weight, 2)
+bmi = round(weight / (height * height))
+
+
+# Example input
+
+# height = 1.75
+# weight = 85
+# Example output 
+# Your BMI is 28, you are slightly overweight.
+
+# height = 1.8
+# weight = 89
+# Example output 
+# Your BMI is 27, you are slightly overweight.
+
+
 
 if bmi < 18.5:
     print(f"Your BMI is {bmi}, you are underweight.")
