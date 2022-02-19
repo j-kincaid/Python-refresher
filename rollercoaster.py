6a7c9f131336c96de71f389f3bb70ea302760597
Jessica Kincaid <4j.kincaid@gmail.com>
2022-02-19 09:44:25 -0600

    add space to question

--- rollercoaster.py
+++ rollercoaster.py
@@ -3,7 +3,7 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
-    age = int(input("What is your age?"))
+    age = int(input("What is your age? "))
     if age < 12:
         print("Please pay $5.")
     elif age > 18:
