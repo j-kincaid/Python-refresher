0ed2a35f4024e60c0374a5c1c757e65c153a2580
Jessica Kincaid <4j.kincaid@gmail.com>
2022-03-15 10:43:56 -0500

    correct password generator loop

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,15 +5,27 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-# nr_symbols = int(input(f"How many symbols would you like?\n"))
-# nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-random_letters = random.randint(1, nr_letters)
-# random_symbols = random.randint(1, nr_symbols)
-# random_numbers = random.randint(1, nr_numbers)
 
-letters_range = range(1, (random_letters + 1))
-letter = random_letters
-for letter in random_letters: 
-    print(letter)
-# print(f"{letters[letters_range]}")
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+# print(password_list)
+random.shuffle(password_list)
+# print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
