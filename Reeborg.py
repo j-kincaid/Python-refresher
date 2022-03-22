63910a2c60d96da714014714e98e33313d0f6310
Jessica Kincaid <4j.kincaid@gmail.com>
2022-03-22 17:38:19 -0500

    add Reborg

---
+++ Reeborg.py
@@ -0,0 +1,37 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def make_square():
+    turn_left()
+    move() 
+    turn_right()
+    move()    
+    turn_right()
+    move()
+    turn_right()
+    move()
+def jump_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    
+def cross_unit(): 
+    jump_hurdle()
+    move()
+    turn_right()
+    move()
+    turn_right() 
+    jump_hurdle()
+    move()
+    turn_right()
+    move()
+    
+cross_unit()
