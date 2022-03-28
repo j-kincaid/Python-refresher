f5fce2a6a83fe62d7f6390ca26f1433095ec2863
Jessica Kincaid <4j.kincaid@gmail.com>
2022-03-28 17:53:11 -0500

    update Reeborg

--- Reeborg.py
+++ Reeborg.py
@@ -13,11 +13,15 @@ def jump():
     move()
     turn_left()
     
-number_of_hurdles = 6
+# number_of_hurdles = 6
 
-while number_of_hurdles > 0: 
-    if at_goal() == False:
-        jump()
-        number_of_hurdles -= 1
+# while number_of_hurdles > 0: 
+#     if at_goal() == False:
+#         jump()
+#         number_of_hurdles -= 1
 
-        
+while not at_goal():
+    jump()
+
+# Use a while loop when you don't care about what number in a sequence
+# you're in, which is different from iterating through a list using a for loop. 
