9bb4e7fb80c3975af08c11d9376cdf596d1cdf99
Jessica Kincaid <4j.kincaid@gmail.com>
2022-03-31 14:37:59 -0500

    update Reeborg

--- Reeborg.py
+++ Reeborg.py
@@ -3,24 +3,16 @@ def turn_right():
     turn_left()
     turn_left()
 
-    
-def jump():
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
-    move()
-    turn_right()
-    while front_is_clear():
-        move()
-    turn_left()
 
-    
 while not at_goal():
-    if wall_in_front():
-        jump()
-    else:
+    if right_is_clear():
+        turn_right()
         move()
+    elif right_is_clear():
+        move()
+    else:
+        turn_left()
+        
 
 # Use a while loop when you don't care about what number in a sequence
 # you're in, which is different from iterating through a list using a for loop. 
