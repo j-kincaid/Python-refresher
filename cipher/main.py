ff026b017c57b0169df4a7e9ec1dea2526bf86db
Jessica Kincaid <4j.kincaid@gmail.com>
2022-04-16 11:49:18 -0500

    more exercises

---
+++ cipher/main.py
@@ -0,0 +1,20 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# name = input("What is your name?")
+# def greet():
+#     print(f"How are you, {name}?")
+#     print("Statement 2")
+#     print("Statement 3")
+# greet()
+
+# name = input("What is your name?\n")
+# location = input("Where are you from?\n")
+
+
+def greet_and_talk(name, location):
+    print(f"How are you, {name}?")
+    print(f"I've heard great things about {location}.")
+greet_and_talk(name = "Juliet", location = "Huntington")
+
---
+++ prime.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+
+from operator import is_
+
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+            print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
