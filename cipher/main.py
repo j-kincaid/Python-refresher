6cc72b860773a06ef3ae2d0fdb45dacefa2091f9
Jessica Kincaid <4j.kincaid@gmail.com>
2022-04-20 10:06:53 -0500

    revert cipher file

--- cipher/main.py
+++ cipher/main.py
@@ -6,15 +6,6 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(text, shift):
-    text_as_list = text.split(', ')
-    i = ''
-    for i in range(0, (text_as_list-1)):
-        i = text_as_list[shift] 
-        print(i)
-    print(f"The encrypted text is {i}.")
-encrypt(text, shift)
-
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
     #plain_text = "hello"
@@ -22,9 +13,8 @@ encrypt(text, shift)
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
 
-# >>> ["foo", "bar", "baz"].index("bar")
-# 1
-
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
