381370276c59c4e977015be1c65bc0f52a31e0be
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-01 14:27:52 -0500

    encode message

--- cipher/main.py
+++ cipher/main.py
@@ -1,20 +1,33 @@
 from difflib import Match
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(text):
-    text_as_list = []
-    for char in range(0, len(text)):
-        text_as_list.append(text[char])
-        print(text_as_list)
-encrypt(text)
+# def encrypt(text):
+#     text_as_list = []
+#     for char in range(0, len(text)):
+#         text_as_list.append(text[char])
+#         print(text_as_list)
+# encrypt(text)
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ''
+    for letter in plain_text:
+        # Use the index() method to find the index of the letter we are looping through in the alphabet
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}.")
+
+encrypt(plain_text=text, shift_amount=shift)
+    
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
