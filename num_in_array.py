60378b0ce9ffced44d92e3f5543ebf110e088aae
Jessica Kincaid <4j.kincaid@gmail.com>
2025-09-15 13:37:39 -0500

    practice for test

---
+++ num_in_array.py
@@ -0,0 +1,9 @@
+"""
+Given an array of numbers and an integer N of a certain value, count the numbers of occurences for every number in the array.
+
+"""
+# Given
+
+N = 5
+A = [3, 4, 4, 6, 1, 4, 4]
+
